33f2a7955c29f6c573fc82e30dc20ab32762676a
Theodore Byron <ted@tedbyron.com>
2018-06-03 04:18:48 -0400

    Update windows install script to symlink whole .atom/ folder to cloned repo

--- .atom/install.ps1
+++ .atom/install.ps1
@@ -4,10 +4,17 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit;
 }
 
-# Create symlink
-New-Item -ItemType SymbolicLink -Path $HOME\.atom -Value $PSScriptRoot -Force;
+# Robocopy files from current .atom\ to cloned repo; include empty dirs but don't replace repo files
+ROBOCOPY $HOME\.atom $PSScriptRoot *.* /E /XC /XN /XO;
+
+# Move .atom\ to .atom.backup\
+Move-Item -Path $HOME\.atom -Destination $HOME\.atom.backup -Force;
+
+# Create symlink from .atom\ to dotfiles\.atom
+New-Item -ItemType SymbolicLink -Path $HOME\.atom -Value $PSScriptRoot;
 
 # Prompt to exit
-Write-Host "Press any key to exit...";
+Write-Host "`n`nYour original .atom\ folder has been moved to .atom.backup\`n" -ForegroundColor Magenta;
+Write-Host "Success! Press any key to exit..." -ForegroundColor Green;
 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
 Exit;
