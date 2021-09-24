4468e9d860affafbba5a5518784153e184e823b8
Teddy Byron <ted@tedbyron.com>
2021-09-23 23:11:50 -0400

    update cargo-outdated.bash

--- scripts/cargo-outdated.bash
+++ scripts/cargo-outdated.bash
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+declare -A current_map
+declare -A next_map
+declare -A install
+declare name
+declare vers
+declare vers_next
+
+declare -r green='\033[32m'   # foreground green
+declare -r yellow='\033[33m'  # foreground yellow
+declare -r nt='\033[0m'       # normal text
 
 # store currently installed binary names and versions in `current_map`
 current_versions() {
@@ -8,69 +18,67 @@ current_versions() {
   local -r loop_len="${#arr[@]}"
   for (( i=0; i < loop_len; i+=2 )); do
     name="${arr[${i}]}"
-    ver="${arr[$(( i+1 ))]}"
-    ver="${ver//[!0-9.]/}"
-    current_map["${name}"]="${ver}"
+    vers="${arr[$(( i+1 ))]}"
+    vers="${vers//[!0-9.]/}"
+    current_map["${name}"]="${vers}"
   done
 }
 
 # store `cargo search` versions in `next_map`
 next_versions() {
   for crate in "${!current_map[@]}"; do
-    ver="$(cargo search --limit 1 "${crate}")"
-    name="$(awk 'NR==1{print $1}' <<<"${ver}")"
-    if [[ -n "${ver}" && "${crate}" == "${name}" ]]; then
-      ver="$(awk 'NR==1{print $3}' <<<"${ver}")"
-      ver="${ver//[!0-9.]/}"
+    vers="$(cargo search --limit 1 "${crate}")"
+    name="$(awk 'NR==1{print $1}' <<<"${vers}")"
+    if [[ -n "${vers}" && "${crate}" == "${name}" ]]; then
+      vers="$(awk 'NR==1{print $3}' <<<"${vers}")"
+      vers="${vers//[!0-9.]/}"
     else
-      ver="${current_map[${crate}]}"
+      vers="${current_map[${crate}]}"
     fi
-    next_map["${crate}"]="${ver}"
+    next_map["${crate}"]="${vers}"
   done
 }
 
 # compare versions and list outdated; get values into `install`
 compare_and_echo() {
   for crate in "${!current_map[@]}"; do
-    ver="${current_map[${crate}]}"
-    ver_next="${next_map[${crate}]}"
-    if [[ "${ver}" != "${ver_next}" ]]; then
-      install["${crate}"]="${ver_next}"
+    vers="${current_map[${crate}]}"
+    vers_next="${next_map[${crate}]}"
+    if [[ "${vers}" != "${vers_next}" ]]; then
+      install["${crate}"]="${vers_next}"
     fi
   done
 
   for crate in "${!install[@]}"; do
-    ver="${current_map[${crate}]}"
-    ver_next="${install[${crate}]}"
-    echo -e "${crate} ${yellow}${ver}${nt} -> ${green}${ver_next}${nt}"
+    vers="${current_map[${crate}]}"
+    vers_next="${install[${crate}]}"
+    echo -e "${crate} ${yellow}${vers}${nt} -> ${green}${vers_next}${nt}"
   done | column -t
 }
 
 # prompt to install outdated crates in `install`
 prompt_and_install() {
+  local
   for crate in "${!install[@]}"; do
-    ver="${install[${crate}]}"
-    echo -en "Install ${crate} v${green}${ver}${nt}? [y/N] "
-    read -r -p '' response
-    case "${response}" in
-        [yY][eE][sS]|[yY]) cargo install -f "${crate}" ;;
-        *) continue ;;
+    vers="${install[${crate}]}"
+    echo -en "Install ${crate} ${green}${vers}${nt}? [y/N] "
+    read -r -p '' y_n
+    case "${y_n}" in
+        [yY][eE][sS]|[yY])
+          read -r -p "Extra args for 'cargo install -f ${crate}': " eargs
+          case "${eargs}" in
+            '') cargo install -f "${crate}" ;;
+            *)  cargo install -f "${eargs}" "${crate}" ;;
+          esac
+          ;;
+        *)
+          continue
+          ;;
     esac
   done
 }
 
 main() {
-  local -A current_map
-  local -A next_map
-  local -A install
-  local name
-  local ver
-  local ver_next
-
-  local -r green='\033[32m'   # foreground green
-  local -r yellow='\033[33m'  # foreground yellow
-  local -r nt='\033[0m'       # normal text
-
   current_versions
   next_versions
   compare_and_echo
