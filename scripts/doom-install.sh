91cef08e56d061b9f4d9a7853518229d46b01b5d
Teddy Byron <ted@tedbyron.com>
2022-04-19 14:11:17 -0400

    update doom install script

--- scripts/doom-install.sh
+++ scripts/doom-install.sh
@@ -9,7 +9,8 @@ brew install d12frosted/emacs-plus/emacs-plus@28 \
 ln -sf /usr/local/opt/emacs-plus@28/Emacs.app /Applications
 brew services restart d12frosted/emacs-plus/emacs-plus@28
 
-gh repo clone hlissner/doom-emacs ~/.config/emacs -- \
+gh repo clone hlissner/doom-emacs ~/.config/emacs \
+    -- \
     --depth 1 \
     --single-branch
 ~/.config/emacs/bin/doom install
