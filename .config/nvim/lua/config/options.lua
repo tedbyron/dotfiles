6d5307cd44eb92d6a0333ce8e286c16a01a212ac
Teddy Byron <ted@tedbyron.com>
2023-03-21 10:57:39 -0400

    add alacritty cfg

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
--- local opt = vim.opt
+local opt = vim.opt
 
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+
+opt.scrolloff = 3
