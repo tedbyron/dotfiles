2da57ae36e16df556da60a7d3584f42845d9a03c
Teddy Byron <ted@tedbyron.com>
2024-01-08 10:55:39 -0500

    update vscode-neovim bindings

--- .config/nvim/lua/config/keymaps/util.lua
+++ .config/nvim/lua/config/keymaps/util.lua
@@ -1,78 +1,11 @@
 local M = {}
 
-local vscode_command_event_name = 'vscode-command'
-local vscode_range_command_event_name = 'vscode-range-command'
-
-local function VSCodeNotify(cmd, ...)
-  vim.rpcnotify(vim.g.vscode_channel, vscode_command_event_name, cmd, ...)
-end
-
-local function VSCodeNotifyRange(cmd, line1, line2, leaveSelection, ...)
-  if not leaveSelection then
-    leaveSelection = vim.NIL
-  end
-  vim.rpcnotify(vim.g.vscode_channel, vscode_range_command_event_name, cmd, line1, line2, 0, 0, leaveSelection, ...)
-end
-
-local function VSCodeNotifyRangePos(cmd, line1, line2, pos1, pos2, leaveSelection, ...)
-  if not leaveSelection then
-    leaveSelection = vim.NIL
-  end
-  vim.rpcnotify(
-    vim.g.vscode_channel,
-    vscode_range_command_event_name,
-    cmd,
-    line1,
-    line2,
-    pos1,
-    pos2,
-    leaveSelection,
-    ...
-  )
-end
-
-local function VSCodeNotifyVisual(cmd, leaveSelection, ...)
-  local mode = vim.fn.mode()
-  if mode == 'V' then
-    local start = vim.fn.line('v')
-    local finish = vim.fn.line('.')
-    VSCodeNotifyRange(cmd, start, finish, leaveSelection, ...)
-  elseif mode == 'v' or mode == '\\<C-v>' then
-    local start = vim.fn.getpos('v')
-    local finish = vim.fn.getpos('.')
-    VSCodeNotifyRangePos(cmd, start, finish, leaveSelection, ...)
-  else
-    VSCodeNotify(cmd, ...)
-  end
-end
-
 ---@param mode string|table
 ---@param lhs string
 ---@param rhs string | function
 ---@param opts table?
 function M.noremap(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, vim.tbl_deep_extend('force', { noremap = true }, opts or {}))
-end
-
----@param mode string|table
----@param lhs string
----@param rhs string
----@param opts table?
-function M.noremap_notify(mode, lhs, rhs, opts)
-  M.noremap(mode, lhs, function()
-    VSCodeNotify(rhs)
-  end, opts)
-end
-
----@param mode string|table
----@param lhs string
----@param rhs string
----@param leaveSelection boolean?
----@param opts table?
-function M.noremap_notify_visual(mode, lhs, rhs, leaveSelection, opts)
-  M.noremap(mode, lhs, function()
-    VSCodeNotifyVisual(rhs, leaveSelection)
-  end, opts)
 end
 
 return M
--- .config/nvim/lua/config/keymaps/vscode.lua
+++ .config/nvim/lua/config/keymaps/vscode.lua
@@ -1,45 +1,55 @@
-local util = require('config.keymaps.util')
+local noremap = require('config.keymaps.util').noremap
+local vscode = require('vscode-neovim')
 
-for _, mode in pairs({ 'n', 'v', 'o' }) do
-  util.noremap(mode, '<Tab>', vim.api.nvim_eval(string.format('maparg("%%", "%s")', mode)))
+---@param mode string|table
+---@param lhs string
+---@param rhs string
+local function noremap_action(mode, lhs, rhs)
+  noremap(mode, lhs, function()
+    vscode.action(rhs)
+  end)
 end
 
-util.noremap_notify('n', '<C-h>', 'workbench.action.focusLeftGroup')
-util.noremap_notify('n', '<C-j>', 'workbench.action.focusBelowGroup')
-util.noremap_notify('n', '<C-k>', 'workbench.action.focusAboveGroup')
-util.noremap_notify('n', '<C-l>', 'workbench.action.focusRightGroup')
+for _, mode in pairs({ 'n', 'v', 'o' }) do
+  noremap(mode, '<Tab>', vim.api.nvim_eval(string.format('maparg("%%", "%s")', mode)))
+end
 
-util.noremap_notify('n', '<Leader> ', 'workbench.action.quickOpen')
-util.noremap_notify('n', '<Leader>;', 'workbench.action.showCommands')
+noremap_action('n', '<C-h>', 'workbench.action.focusLeftGroup')
+noremap_action('n', '<C-j>', 'workbench.action.focusBelowGroup')
+noremap_action('n', '<C-k>', 'workbench.action.focusAboveGroup')
+noremap_action('n', '<C-l>', 'workbench.action.focusRightGroup')
+
+noremap_action('n', '<Leader> ', 'workbench.action.quickOpen')
+noremap_action('n', '<Leader>;', 'workbench.action.showCommands')
 vim.keymap.set('n', '<Leader>w', '<C-w>')
 
 vim.keymap.set({ 'n', 'x', 'o' }, 'gc', '<Plug>VSCodeCommentary')
 vim.keymap.set('n', 'gcc', '<Plug>VSCodeCommentaryLine')
 
-util.noremap_notify('n', '<Leader>ea', 'editor.action.quickFix')
-util.noremap_notify('n', '<Leader>es', 'editor.action.triggerSuggest')
-util.noremap_notify('n', '<Leader>ef', 'editor.action.formatDocument')
-util.noremap_notify_visual('x', '<Leader>ef', 'editor.action.formatSelection')
-util.noremap_notify('n', '<Leader>er', 'editor.action.rename')
+noremap_action('n', '<Leader>ea', 'editor.action.quickFix')
+noremap_action('n', '<Leader>es', 'editor.action.triggerSuggest')
+noremap_action('n', '<Leader>ef', 'editor.action.formatDocument')
+noremap_action('x', '<Leader>ef', 'editor.action.formatSelection')
+noremap_action('n', '<Leader>er', 'editor.action.rename')
 
-util.noremap_notify('n', '<Leader>ga', 'git.stage')
-util.noremap_notify('n', '<Leader>gA', 'git.stageAll')
-util.noremap_notify('n', '<Leader>gd', 'git.openChange')
-util.noremap_notify('n', '<Leader>gr', 'git.rename')
-util.noremap_notify('n', '<Leader>gu', 'git.unstage')
-util.noremap_notify_visual({ 'n', 'x' }, '<Leader>ghs', 'git.stageSelectedRanges')
-util.noremap_notify_visual({ 'n', 'x' }, '<Leader>ghu', 'git.unstageSelectedRanges')
-util.noremap_notify_visual({ 'n', 'x' }, '<Leader>ghr', 'git.revertSelectedRanges')
+noremap_action('n', '<Leader>ga', 'git.stage')
+noremap_action('n', '<Leader>gA', 'git.stageAll')
+noremap_action('n', '<Leader>gd', 'git.openChange')
+noremap_action('n', '<Leader>gr', 'git.rename')
+noremap_action('n', '<Leader>gu', 'git.unstage')
+noremap_action({ 'n', 'x' }, '<Leader>ghs', 'git.stageSelectedRanges')
+noremap_action({ 'n', 'x' }, '<Leader>ghu', 'git.unstageSelectedRanges')
+noremap_action({ 'n', 'x' }, '<Leader>ghr', 'git.revertSelectedRanges')
 
-util.noremap_notify('n', '<Leader>un', 'notifications.showList')
-util.noremap_notify('n', '<Leader>uN', 'notifications.clearAll')
-util.noremap_notify('n', '<Leader>ur', 'workbench.action.reloadWindow')
+noremap_action('n', '<Leader>un', 'notifications.showList')
+noremap_action('n', '<Leader>uN', 'notifications.clearAll')
+noremap_action('n', '<Leader>ur', 'workbench.action.reloadWindow')
 
-util.noremap_notify('n', ']d', 'editor.action.marker.next')
-util.noremap_notify('n', '[d', 'editor.action.marker.prev')
-util.noremap_notify('n', ']h', 'workbench.action.editor.nextChange')
-util.noremap_notify('n', '[h', 'workbench.action.editor.previousChange')
-util.noremap_notify('n', ']t', 'todo-tree.goToNext')
-util.noremap_notify('n', '[t', 'todo-tree.goToPrevious')
-util.noremap_notify('n', ']b', 'workbench.action.nextEditor')
-util.noremap_notify('n', '[b', 'workbench.action.previousEditor')
+noremap_action('n', ']d', 'editor.action.marker.next')
+noremap_action('n', '[d', 'editor.action.marker.prev')
+noremap_action('n', ']h', 'workbench.action.editor.nextChange')
+noremap_action('n', '[h', 'workbench.action.editor.previousChange')
+noremap_action('n', ']t', 'todo-tree.goToNext')
+noremap_action('n', '[t', 'todo-tree.goToPrevious')
+noremap_action('n', ']b', 'workbench.action.nextEditor')
+noremap_action('n', '[b', 'workbench.action.previousEditor')
