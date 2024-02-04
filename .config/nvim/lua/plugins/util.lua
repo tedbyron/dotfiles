6166aa211e509dd6e6e104e1ce2217ec181f86c7
Teddy Byron <ted@tedbyron.com>
2024-02-03 23:41:45 -0500

    add discord presence

---
+++ .config/nvim/lua/plugins/util.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'andweeb/presence.nvim',
+    lazy = false,
+    vscode = true,
+    opts = {
+      neovim_image_text = 'I wish it was Emacs',
+      enable_line_number = true,
+      buttons = false,
+      workspace_text = nil,
+    },
+  },
+}
