3640d5858f10fba639eeaef94cbd89a385f25df0
Teddy Byron <ted@tedbyron.com>
2024-02-11 02:48:19 -0500

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,13 @@
+function Invoke-Starship-PreCommand {
+  $loc = $executionContext.SessionState.Path.CurrentLocation
+  $prompt = "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $host.ui.Write($prompt)
+}
+
 $ENV:STARSHIP_CONFIG = "$HOME\git\dotfiles\.config\starship.toml"
 Invoke-Expression (&starship init powershell)
+
 Set-Alias -Name iexs -Value iex.bat
