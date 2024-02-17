60ee5cff74d6a0e67a44fe8193a2bd7ba05f51ee
Teddy Byron <ted@tedbyron.com>
2024-02-16 23:54:42 -0500

    Update IEx config

--- .iex.exs
+++ .iex.exs
@@ -1,7 +1,4 @@
-# iex_config = Path.expand("~/.iex.exs")
-# if iex_config |> File.exists?() do
-#   import iex_config
-# end
+# import_file_if_available("~/.iex.exs")
 
 # :observer.start/0
 # runtime_info/0 runtime_info/1
@@ -10,6 +7,10 @@
 defmodule ColorHelpers do
   @ansi Application.compile_env(:elixir, :ansi_enabled, true)
 
+  @spec ansi_enabled?() :: boolean()
+  def ansi_enabled?, do: @ansi
+
+  @spec format(String.t(), atom()) :: String.t()
   def format(string, color) do
     if @ansi do
       IO.ANSI.format([color, string]) |> IO.chardata_to_string()
@@ -18,6 +19,7 @@ defmodule ColorHelpers do
     end
   end
 
+  @spec puts(String.t(), atom()) :: atom()
   def puts(string, color \\ :default_color) do
     IO.puts(format(string, color))
     IEx.dont_display_result()
@@ -27,10 +29,12 @@ end
 defmodule H do
   import ColorHelpers
 
+  @ansi ansi_enabled?()
   @home System.user_home!() |> Path.expand()
   @path_separator if elem(:os.type(), 0) == :unix, do: "/", else: "\\"
 
   # Format the current working directory like fish shell.
+  @spec cwd() :: String.t()
   defp cwd do
     case File.cwd() do
       {:ok, cwd} ->
@@ -49,9 +53,11 @@ defmodule H do
     end
   end
 
+  @spec cwd_color() :: String.t()
   defp cwd_color, do: format(cwd(), :cyan)
 
   # Color the node name and host yellow.
+  @spec node_color() :: String.t()
   defp node_color do
     node()
     |> to_string()
@@ -63,19 +69,35 @@ defmodule H do
   @caret "\uf0da"
   @caret_color format(@caret, :blue)
   @bullet "\u2219"
-  @bullet format(@bullet, :light_black)
+  @bullet_color format(@bullet, :light_black)
 
-  def default_prompt, do: "#{cwd_color()} #{@caret_color}"
-  def alive_prompt, do: "#{cwd_color()} #{node_color()} #{@caret_color}"
+  @doc "Build a prompt from the type passed."
+  @spec prompt(atom()) :: String.t()
+  @spec prompt(atom(), boolean()) :: String.t()
+  def prompt(type, ansi \\ @ansi) do
+    case type do
+      :default ->
+        if ansi, do: "#{cwd_color()} #{@caret_color}", else: "#{cwd()} #{@caret}"
 
-  def continuation_prompt do
-    @bullet <> String.duplicate(" ", String.length("#{cwd()} #{@caret}") - 1)
+      :alive ->
+        if ansi do
+          "[#{node_color()}] #{cwd_color()} #{@caret_color}"
+        else
+          "[#{node()}] #{cwd()} #{@caret}"
+        end
+
+      continuation ->
+        case continuation do
+          :continuation -> prompt(:default, false)
+          :alive_continuation -> prompt(:alive, false)
+        end
+        |> String.length()
+        |> then(&String.duplicate(" ", &1 - 1))
+        |> then(&if @ansi, do: @bullet_color <> &1, else: @bullet <> &1)
+    end
   end
 
-  def alive_continuation_prompt do
-    @bullet <> String.duplicate(" ", String.length("#{cwd()} #{node()} #{@caret}") - 1)
-  end
-
+  @spec format_app_version(any(), atom(), charlist(), boolean()) :: String.t()
   defp format_app_version(app, color, vsn, puts) do
     s = (to_string(app) |> format(color)) <> format(": #{vsn}", :light_black)
 
@@ -89,14 +111,20 @@ defmodule H do
 
   @apps Application.loaded_applications()
 
-  # If `app` is running, returns its name in `color` and version.
+  @doc "If `app` is running, returns its name and version."
+  @spec app_version(atom()) :: String.t() | atom()
+  @spec app_version(atom(), atom()) :: String.t() | atom()
+  @spec app_version(atom(), atom(), boolean()) :: String.t() | atom()
   def app_version(app, color \\ :default_color, puts \\ true) do
     with {_, _, vsn} <- Enum.find(@apps, &(elem(&1, 0) == app)) do
       format_app_version(app, color, vsn, puts)
     end
   end
 
-  # If inside a mix app, puts its name and version.
+  @doc "If inside a mix app, puts its name and version."
+  @spec mix_app_version() :: String.t() | atom()
+  @spec mix_app_version(atom()) :: String.t() | atom()
+  @spec mix_app_version(atom(), boolean()) :: String.t() | atom()
   def mix_app_version(color \\ :default_color, puts \\ true) do
     if function_exported?(Mix.Project, :config, 0) do
       config = Mix.Project.config()
@@ -108,8 +136,11 @@ defmodule H do
     end
   end
 
-  # Extension of `IEx.Helpers.cd/1` to support `to_string/1` types and the `-` param. Updates the
-  # `PWD` and `OLDPWD` environment variables, and IEx prompts.
+  @doc """
+  Extension of `IEx.Helpers.cd/1` to support `to_string/1` types and the `-` param. Updates the
+  `PWD` and `OLDPWD` environment variables, and IEx prompts.
+  """
+  @spec cd(any()) :: atom()
   def cd(directory) do
     directory =
       to_string(directory)
@@ -144,6 +175,7 @@ defmodule H do
     IEx.dont_display_result()
   end
 
+  @spec d(any()) :: atom()
   defdelegate d(directory), to: __MODULE__, as: :cd
 end
 
