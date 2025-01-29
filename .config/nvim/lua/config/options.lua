5d0b547c113d54384417b21aabf34cdc2ef4ce78
Teddy Byron <ted@ted.ooo>
2025-01-29 10:32:16 -0500

    Update nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,5 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-vim.g.lazygit_floating_window_use_plenary = true
-vim.g.lazyvim_picker = 'telescope'
 vim.g.loaded_node_provider = false
 vim.g.loaded_perl_provider = false
 vim.g.loaded_python3_provider = false
