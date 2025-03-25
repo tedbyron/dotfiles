5d3a4c3ab4c2c7fd8e169507f102ded16e518a9b
Teddy Byron <ted@ted.ooo>
2025-03-25 15:41:35 -0400

    Add nvim remap fsearch very magic opt

--- .config/nvim/lua/config/keymaps/init.lua
+++ .config/nvim/lua/config/keymaps/init.lua
@@ -4,6 +4,7 @@ local noremap = require('config.keymaps.util').noremap
 
 noremap('n', '0', '^')
 noremap('n', '^', '0')
+noremap('n', '/', '/\\v')
 
 for _, mode in pairs({ 'n', 'v', 'o' }) do
   noremap(mode, '<Tab>', vim.api.nvim_eval(string.format('maparg("%%", "%s")', mode)))
