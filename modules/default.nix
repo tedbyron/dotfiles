890ad2e3ca03fc142b3f5d390a94a42ab3c92c55
Teddy Byron <ted@ted.ooo>
2025-04-26 19:40:39 -0400

    Removed shared modules folder

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,50 @@
-{ darwin, ... }:
+{
+  self,
+  pkgs,
+  darwin,
+  ...
+}:
 {
   imports = [
     (if darwin then ./darwin else ./nixos)
-    ./shared
+    ./nix.nix
   ];
+
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  fonts.packages = with pkgs; [
+    self.outputs.packages.${pkgs.system}.curlio-ttf
+    inter
+    libre-baskerville
+  ];
+
+  programs = {
+    gnupg.agent.enable = true;
+    zsh.enable = true;
+  };
+
+  environment = {
+    variables.LANG = "en_US.UTF-8";
+
+    shells = with pkgs; [
+      bashInteractive
+      zsh
+    ];
+
+    systemPackages = with pkgs; [
+      binutils
+      coreutils
+      curl
+      diffutils
+      findutils
+      gawk
+      gnugrep
+      gnused
+      groff
+      less
+      nmap
+      openssl
+      python3
+    ];
+  };
 }
