c593cf1874497bf0bb2a60e7ad9d5258c2bb264f
Teddy Byron <ted@ted.ooo>
2025-05-01 13:50:18 -0400

    Fix build

--- users/ted/programs/default.nix
+++ users/ted/programs/default.nix
@@ -109,7 +109,7 @@
 
     neovim = {
       enable = true;
-      package = unstable.neovim-unwrapped;
+      package = unstable.neovim-unwrapped.overrideAttrs { inherit (pkgs.neovim-unwrapped) meta; }; # TODO: 25.05
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
