6c29736cb8ef9d99aa400591095c0cb7c85ab621
Teddy Byron <ted@ted.ooo>
2025-05-04 15:56:39 -0400

    Update hyprland keybinds

--- users/ted/modules/hyprland.nix
+++ users/ted/modules/hyprland.nix
@@ -188,7 +188,7 @@ in
         };
 
         plugin = [
-          { hyprsplit.num_workspaces = 10; }
+          { hyprsplit.num_workspaces = 4; }
           {
             dynamic-cursors = {
               mode = "stretch";
@@ -203,46 +203,48 @@ in
 
         bind = [
           "SUPER, grave, exec, $terminal"
-          "SUPER, Q, killactive,"
-          "SUPER, M, exit,"
-          "SUPER, E, exec, $fileManager"
-          "Alt_L, space, exec, $menu"
-          "SUPER, J, togglesplit, # dwindle"
-          "SUPER, D, split:swapactiveworkspaces, current +1"
-          "SUPER, G, split:grabroguewindows"
+          "SUPER, m,     exit,"
+          "SUPER, f,     exec, $fileManager"
+          "SUPER, s,     togglesplit,"
+          "SUPER, d,     split:swapactiveworkspaces, current +1"
+          "SUPER, g,     split:grabroguewindows"
+          "ALT,   space, exec, $menu"
+          "ALT,   f4,    killactive,"
 
-          "SUPER, left, movefocus, l"
-          "SUPER, right, movefocus, r"
-          "SUPER, up, movefocus, u"
-          "SUPER, down, movefocus, d"
+          "SUPER, n, movefocus, l"
+          "SUPER, e, movefocus, d"
+          "SUPER, i, movefocus, u"
+          "SUPER, o, movefocus, r"
+
+          "SUPER SHIFT, n, workspace, e-1"
+          "SUPER SHIFT, e, togglespecialworkspace, magic"
+          "SUPER SHIFT, i, movetoworkspace, special:magic"
+          "SUPER SHIFT, o, workspace, e+1"
+
+          "SUPER, mouse_down, workspace, e+1"
+          "SUPER, mouse_up, workspace, e-1"
 
           "SUPER, 1, split:workspace, 1"
           "SUPER, 2, split:workspace, 2"
           "SUPER, 3, split:workspace, 3"
           "SUPER, 4, split:workspace, 4"
-          "SUPER, 5, split:workspace, 5"
-          "SUPER, 6, split:workspace, 6"
-          "SUPER, 7, split:workspace, 7"
-          "SUPER, 8, split:workspace, 8"
-          "SUPER, 9, split:workspace, 9"
-          "SUPER, 0, split:workspace, 10"
+          # "SUPER, 5, split:workspace, 5"
+          # "SUPER, 6, split:workspace, 6"
+          # "SUPER, 7, split:workspace, 7"
+          # "SUPER, 8, split:workspace, 8"
+          # "SUPER, 9, split:workspace, 9"
+          "SUPER, 0, split:workspace, 1"
 
           "SUPER SHIFT, 1, split:movetoworkspacesilent, 1"
           "SUPER SHIFT, 2, split:movetoworkspacesilent, 2"
           "SUPER SHIFT, 3, split:movetoworkspacesilent, 3"
           "SUPER SHIFT, 4, split:movetoworkspacesilent, 4"
-          "SUPER SHIFT, 5, split:movetoworkspacesilent, 5"
-          "SUPER SHIFT, 6, split:movetoworkspacesilent, 6"
-          "SUPER SHIFT, 7, split:movetoworkspacesilent, 7"
-          "SUPER SHIFT, 8, split:movetoworkspacesilent, 8"
-          "SUPER SHIFT, 9, split:movetoworkspacesilent, 9"
-          "SUPER SHIFT, 0, split:movetoworkspacesilent, 10"
-
-          "SUPER, S, togglespecialworkspace, magic"
-          "SUPER SHIFT, S, movetoworkspace, special:magic"
-
-          "SUPER, mouse_down, workspace, e+1"
-          "SUPER, mouse_up, workspace, e-1"
+          # "SUPER SHIFT, 5, split:movetoworkspacesilent, 5"
+          # "SUPER SHIFT, 6, split:movetoworkspacesilent, 6"
+          # "SUPER SHIFT, 7, split:movetoworkspacesilent, 7"
+          # "SUPER SHIFT, 8, split:movetoworkspacesilent, 8"
+          # "SUPER SHIFT, 9, split:movetoworkspacesilent, 9"
+          "SUPER SHIFT, 0, split:movetoworkspacesilent, 1"
         ];
 
         bindm = [
@@ -251,19 +253,19 @@ in
         ];
 
         bindel = [
-          ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-          ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+          ",XF86AudioRaiseVolume,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+          ",XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ",XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ",XF86MonBrightnessUp,   exec, brightnessctl s 10%+"
           ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
         ];
 
         bindl = [
-          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioNext,  exec, playerctl next"
           ", XF86AudioPause, exec, playerctl play-pause"
-          ", XF86AudioPlay, exec, playerctl play-pause"
-          ", XF86AudioPrev, exec, playerctl previous"
+          ", XF86AudioPlay,  exec, playerctl play-pause"
+          ", XF86AudioPrev,  exec, playerctl previous"
         ];
 
         cursor = {
